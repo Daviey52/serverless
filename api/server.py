3b584a8d11e163ef2e08860a10d4ef2096f98940
Daviey52 <davidkariska@gmail.com>
2022-02-03 02:18:18 -0800

    up

--- api/server.py
+++ api/server.py
@@ -1,28 +1,27 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
-import requests
+#from urllib import parse
+#import requests
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    url_path = self.path
-    url_components = parse.urlsplit(url_path)
-    query_string_list = parse.parse_qsl(url_components.query)
-    dic = dict(query_string_list)
+    #url_path = self.path
+    #url_components = parse.urlsplit(url_path)
+    #query_string_list = parse.parse_qsl(url_components.query)
+    #dic = dict(query_string_list)
 
-    if 'lang' in dic:
-      url = "https://fourtonfish.com/hellosalut/?lang"
-      r = requests.get(url + dic['lang'])
-      message = str(r)
-    else:
-      message = "Please provide a valid language abbriviation"
+    #if 'lang' in dic:
+      #url = "https://fourtonfish.com/hellosalut/?lang"
+      #r = requests.get(url + dic['lang'])
+      #message = str(r)
+    #else:
+      #message = "Please provide a valid language abbriviation"
     
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
+    message = 'Hello'
 
     self.wfile.write(message.encode())
-
-    return
 
 
